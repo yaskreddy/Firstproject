24236b7604d64ad6d777175af4527272409df9ee
kishore <adisivakishore@gmail.com>
2017-11-07 10:19:58 +0530

    project files

--- music/views.py
+++ music/views.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h1>This is the Music app home page<\h1>")
